c2a80d2f473fc6cc2ecacc943b46bc58ab96d2ce
Rahim Halani <rh21597@hotmail.com>
2021-02-07 17:47:15 +0100

    Initial commit.
    This is first commit to the repo. It has images and all files that are uploaded to the website.

---
+++ script/menu.js
@@ -0,0 +1,9 @@
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px"
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
