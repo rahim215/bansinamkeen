498b832b267b7d651dd0857828e005de3f4edb33
Rahim Halani <rh21597@hotmail.com>
2021-02-08 20:59:00 +0100

    image convertion to small size

--- mail.php
+++ mail.php
@@ -1,14 +1,9 @@
 <?php
-         $to = "xyz@somedomain.com";
-         $subject = "This is subject";
+         $to = "support@bansinamkeen.com";
+         $subject = "E-Mail from website.";
 
          $message = "<b>This is HTML message.</b>";
          $message .= "<h1>This is headline.</h1>";
-
-         $header = "From:abc@somedomain.com \r\n";
-         $header .= "Cc:afgh@somedomain.com \r\n";
-         $header .= "MIME-Version: 1.0\r\n";
-         $header .= "Content-type: text/html\r\n";
 
          $retval = mail ($to,$subject,$message,$header);
 
